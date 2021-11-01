90c4804ce42044674147549b7128fe88050ea074
Alex Novikov <aleksey.novikov@pimpay.ru>
2021-11-01 18:43:17 +0300

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chrome extension yandex music ad blocker
+1. Turn off sound if ad is playing and turn on if ad is end
+
+2. Hide popup which blocks playing and continue play music
+
+## Screenshots
+### Enable extension
+![Main screen](preview/enable_extension.png)
+
+### Start / stop extension
+![Settings](preview/extension.png)
+
+### Turn off sound while ad is playing
+![Settings](preview/log.png)
---
+++ background.js
@@ -0,0 +1,64 @@
+const App = () => {
+    const preffix = 'yamusic-ad-blocker'
+    const PERIOD = 2000
+    let isAdPlaying = false
+    let interval = null
+
+    const playCntrl = document.querySelector('.player-controls__btn_play')
+    const play = () => playCntrl.dispatchEvent(new Event('click'))
+    const playIfPause = () => (playCntrl.classList.contains('player-controls__btn_pause') === false ? play() : null)
+    const sound = () => document.querySelector('.volume__btn').dispatchEvent(new Event('click'))
+    const isAd = () => !!(document.querySelector('.audio-advert').classList.contains('audio-advert_hidden') === false && document.querySelector('.audio-advert__content').innerHTML.length > 0)
+    const isPopup = () => {
+        const popup = document.querySelector('.crackdown-popup')
+        const overlay = document.querySelector('.deco-pane-overlay')
+        return !!(popup && overlay && popup.classList.contains('popup_hidden') === false && overlay.classList.contains('overlay_shown') === true)
+    }
+    const now = () => new Intl.DateTimeFormat('ru', { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' }).format(new Date())
+    const log = (msg) => console.log(`${preffix} ${now()}: ${msg}`)
+    const actions = () => {
+        if ((isAd() && !isAdPlaying) || (!isAd() && isAdPlaying)) {
+            sound()
+            isAdPlaying = !isAdPlaying
+            if (!isAdPlaying) playIfPause()
+            log(isAdPlaying ? 'Ad: Sound off' : 'Ad end: Sound on')
+        }
+        if (isPopup()) {
+            const popup = document.querySelector('.crackdown-popup')
+            const overlay = document.querySelector('.deco-pane-overlay')
+            popup.classList.add('popup_hidden')
+            overlay.classList.remove('overlay_shown')
+            if (!isAdPlaying) playIfPause()
+            log('Blocking popup: Close and start playing')
+        }
+    }
+    const checkUrl = () => /^https:\/\/music.yandex.ru/.test(window.location.href)
+
+    const start = () => {
+        log('start')
+        window.yaMusicAdBlockerInterval = setInterval(actions, PERIOD);
+        window.yaMusicAdBlockerStatus = 'start'
+    }
+    const stop = () => {
+        log('stop')
+        clearInterval(window.yaMusicAdBlockerInterval)
+        window.yaMusicAdBlockerStatus = 'stop'
+    }
+    if (!checkUrl()) {
+        console.error(`${preffix} Extension works only on https://music.yandex.ru`)
+        return
+    } else {
+        if (window.yaMusicAdBlockerStatus === 'start') stop()
+        else start()
+    }
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    const tabId = tab.id
+    chrome.scripting.executeScript({
+        target: {
+            tabId
+        },
+        function: App
+    })
+})
